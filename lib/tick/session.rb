bdb34ea0bef75d6a1718201747914159ce811695
Brian Pattison <brian@brianpattison.com>
2014-05-30 13:19:15 -0500

    Fix issue with #logged_in?

--- lib/tick/session.rb
+++ lib/tick/session.rb
@@ -1,84 +1,84 @@
 module Tick
-  
+
   class AuthenticationError < Exception
   end
-  
+
   class Session < Tick::Base
     attr_accessor :company, :email, :first_name, :last_name, :password
-  
+
     SERVICE_NAME = "Tick"
-  
+
     def company
       @company ||= storage.objectForKey("company")
     end
-  
+
     def company=(value)
       @company = value
       storage.setObject(value, forKey:"company")
       storage.synchronize
       @company
     end
-    
+
     def destroy
       storage.removeObjectForKey("company")
       storage.removeObjectForKey("email")
       SSKeychain.deletePasswordForService(SERVICE_NAME, account:email)
       self.class.current = nil
     end
-  
+
     def email
       @email ||= storage.objectForKey("email")
     end
-  
+
     def email=(value)
       @email = value
       storage.setObject(value, forKey:"email")
       storage.synchronize
       @email
     end
-    
+
     def first_name
       @first_name ||= storage.objectForKey("first_name")
     end
-  
+
     def first_name=(value)
       @first_name = value
       storage.setObject(value, forKey:"first_name")
       storage.synchronize
       @first_name
     end
-    
+
     def last_name
       @last_name ||= storage.objectForKey("last_name")
     end
-  
+
     def last_name=(value)
       @last_name = value
       storage.setObject(value, forKey:"last_name")
       storage.synchronize
       @last_name
     end
-  
+
     def password
       @password ||= SSKeychain.passwordForService(SERVICE_NAME, account:email)
     end
-  
+
     def password=(value)
       @password = value
       SSKeychain.setPassword(value, forService:SERVICE_NAME, account:email)
       @password
     end
-  
+
     def storage
       NSUserDefaults.standardUserDefaults
     end
-    
+
     def self.create(params, &block)
       url  = "https://#{params[:company]}.tickspot.com/api/users"
-      
+
       company = params[:company]
       params.delete(:company)
-  
+
       request_manager.GET(url, parameters:params, success:lambda{|operation, result|
         # TODO: Save first and last name
         @current = new
@@ -90,22 +90,22 @@ module Tick
         ap error
         block.call(nil) if block
       })
-      
+
       self
     end
-  
+
     def self.current
-      @current || (logged_in? ? new : nil)
+      @current || new
     end
-    
+
     def self.current=(value)
       @current = value
     end
-    
+
     def self.logged_in?
-      (@current && @current.company && @current.email && @current.password) ? true : false
+      (current.company && current.email && current.password) ? true : false
     end
-  
+
   end
-  
+
 end
